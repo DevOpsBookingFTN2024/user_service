675fef2d9ee83449088a4ac8d9598fec3f9df879
Nikola Jovic <jovnikola99@gmail.com>
2024-12-20 19:48:29 +0100

    feat: Add response classes

--- src/main/java/uns/ac/rs/user_service/dto/response/ApiResponse.java
+++ src/main/java/uns/ac/rs/user_service/dto/response/ApiResponse.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class MessageResponse {
+public class ApiResponse<T> {
+    private T data;
     private String message;
 }
---
+++ src/main/java/uns/ac/rs/user_service/dto/response/JwtResponse.java
@@ -0,0 +1,32 @@
+package uns.ac.rs.user_service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+    private String token;
+
+    private String type = "Bearer";
+
+    private UUID id;
+
+    private String username;
+
+    private String emailAddress;
+
+    private List<String> roles;
+
+    public JwtResponse(String accessToken, UUID id, String username, String emailAddress, List<String> roles) {
+        this.token = accessToken;
+        this.id = id;
+        this.username = username;
+        this.emailAddress = emailAddress;
+        this.roles = roles;
+    }
+}
