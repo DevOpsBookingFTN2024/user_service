2906e6848a78e5628488edd290a456bb33b6e2d7
Nikola Jovic <jovnikola99@gmail.com>
2024-12-20 13:12:29 +0100

    feat: Add repository classes

--- src/main/java/uns/ac/rs/user_service/controller/UserController.java
+++ src/main/java/uns/ac/rs/user_service/controller/UserController.java
@@ -5,34 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uns.ac.rs.user_service.dto.UserDTO;
-import uns.ac.rs.user_service.dto.request.CreateUserRequest;
 import uns.ac.rs.user_service.dto.response.MessageResponse;
 import uns.ac.rs.user_service.service.UserService;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    @Autowired
-    private UserService userService;
 
-    @PostMapping("/create")
-    public ResponseEntity<?> createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
-        MessageResponse messageResponse = userService.createUser(createUserRequest);
-        return ResponseEntity.ok(messageResponse);
-    }
-
-    @GetMapping("/all")
-    public ResponseEntity<?> getAllUsers() {
-        List<UserDTO> users = userService.getAllUsers();
-        return ResponseEntity.ok(users);
-    }
-
-    @GetMapping("/{userId}")
-    public ResponseEntity<?> getUserById(@PathVariable UUID userId) {
-        UserDTO user = userService.getUserById(userId);
-        return ResponseEntity.ok(user);
-    }
 }
---
+++ src/main/java/uns/ac/rs/user_service/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package uns.ac.rs.user_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uns.ac.rs.user_service.model.ERole;
+import uns.ac.rs.user_service.model.Role;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Optional<Role> findByName(ERole name);
+    boolean existsByName(ERole name);
+
+}
