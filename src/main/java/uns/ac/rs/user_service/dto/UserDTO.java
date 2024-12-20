4a82a7fa6240b8bf698afcb45844f937229c2b81
Ruzica74 <ruzicaa997@gmail.com>
2024-12-20 20:50:25 +0100

    feat: add dto

--- src/main/java/uns/ac/rs/user_service/dto/UserDTO.java
+++ src/main/java/uns/ac/rs/user_service/dto/UserDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -14,4 +15,14 @@ public class UserDTO {
     private UUID id;
 
     private String username;
+
+    private String emailAddress;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String residence;
+
+    private Set<String> roles;
 }
