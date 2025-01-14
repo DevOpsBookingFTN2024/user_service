1460e4454ff7989cedf4adece89bd51da9881eee
Nikola Jovic <jovnikola99@gmail.com>
2025-01-14 22:29:09 +0100

    fix: Fix token duration

--- src/main/java/uns/ac/rs/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/uns/ac/rs/user_service/exception/GlobalExceptionHandler.java
@@ -28,6 +28,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGenericException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse("An unexpected error occurred."));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new MessageResponse("An error occurred: " + ex.getMessage()));
     }
 }
