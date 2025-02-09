63ca5b5a6a063ea07fba28a520919f347e8deee6
Nikola Jovic <jovnikola99@gmail.com>
2025-02-09 20:32:04 +0100

    fix: Add logging to controllers (#48)

--- src/main/java/uns/ac/rs/user_service/controller/AuthController.java
+++ src/main/java/uns/ac/rs/user_service/controller/AuthController.java
@@ -1,6 +1,7 @@
 package uns.ac.rs.user_service.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import uns.ac.rs.user_service.dto.response.MessageResponse;
 import uns.ac.rs.user_service.service.AuthService;
 
 @CrossOrigin(origins = "*")
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -19,13 +21,17 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody RegistrationRequest registrationRequest) {
+        log.info("Received registration request for username: {}", registrationRequest.getUsername());
         MessageResponse messageResponse = authService.registerUser(registrationRequest);
+        log.info("User {} registered successfully.", registrationRequest.getUsername());
         return ResponseEntity.ok(messageResponse);
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        log.info("Authentication attempt for username: {}", loginRequest.getUsername());
         JwtResponse jwtResponse = authService.authenticateUser(loginRequest);
+        log.info("User {} authenticated successfully.", loginRequest.getUsername());
         return ResponseEntity.ok(jwtResponse);
     }
 }
--- src/main/java/uns/ac/rs/user_service/controller/UserController.java
+++ src/main/java/uns/ac/rs/user_service/controller/UserController.java
@@ -1,6 +1,7 @@
 package uns.ac.rs.user_service.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import uns.ac.rs.user_service.dto.response.MessageResponse;
 import uns.ac.rs.user_service.service.UserService;
 
 @CrossOrigin(origins = "*")
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -19,32 +21,42 @@ public class UserController {
 
     @GetMapping("/me")
     public ResponseEntity<?> getCurrentUser() {
+        log.info("Fetching current user details.");
         UserDTO currentUser = userService.getCurrentUser();
+        log.info("Current user details fetched successfully: {}", currentUser.getUsername());
         return ResponseEntity.ok(currentUser);
     }
 
     @GetMapping("/{username}")
     public ResponseEntity<?> getUserByUsername(@PathVariable String username) {
+        log.info("Fetching user by username: {}", username);
         UserDTO user = userService.getUserByUsername(username);
+        log.info("User details retrieved successfully for username: {}", username);
         return ResponseEntity.ok(user);
     }
 
     @PutMapping("/update")
     public ResponseEntity<?> updateCurrentUser(@Valid @RequestBody UserUpdateRequest userUpdateRequest) {
+        log.info("Updating user: {}", userUpdateRequest.getUsername());
         MessageResponse messageResponse = userService.updateCurrentUser(userUpdateRequest);
+        log.info("User {} updated successfully.", userUpdateRequest.getUsername());
         return ResponseEntity.ok(messageResponse);
     }
 
     @PutMapping("/change-password")
     public ResponseEntity<?> changePassword(@Valid @RequestBody PasswordChangeRequest passwordChangeRequest) {
+        log.info("Changing password current user.");
         MessageResponse messageResponse = userService.changePassword(passwordChangeRequest);
+        log.info("Password changed successfully for current user.");
         return ResponseEntity.ok(messageResponse);
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<?> deleteCurrentUser(@RequestHeader("Authorization") String authorizationHeader) {
         String jwtToken = authorizationHeader.replace("Bearer ", "");
+        log.info("Deleting current user associated with token.");
         MessageResponse messageResponse = userService.deleteCurrentUser(jwtToken);
+        log.info("User successfully deleted.");
         return ResponseEntity.ok(messageResponse);
     }
 }
